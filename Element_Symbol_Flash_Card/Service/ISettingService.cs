a98b7846289a89795a8d1cc284d70a5f5d73f17e
kaminaka rikuya <rikupin1105@gmail.com>
2022-07-02 16:22:09 +0900

    Create ISettingService.cs

---
+++ Element_Symbol_Flash_Card/Service/ISettingService.cs
@@ -0,0 +1,13 @@
+﻿using Element_Symbol_Flash_Card.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Element_Symbol_Flash_Card.Service
+{
+    public interface ISettingService
+    {
+        void Save(Setting setting);
+        Setting GetSetting();
+    }
+}
