2de86be6dc5ca6989b0dfad0dd316331370f8364
kaminaka rikuya <rikupin1105@gmail.com>
2022-07-02 16:22:10 +0900

    Create Setting.cs

---
+++ Element_Symbol_Flash_Card/Model/Setting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Element_Symbol_Flash_Card.Model
+{
+    public class Setting
+    {
+        public bool Random { get; set; }
+        public bool IsDisplayedElementNumber { get; set; }
+    }
+}
